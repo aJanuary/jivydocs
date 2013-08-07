ad0ad63406ce9439d351843d45b40c8f24391d73
Andrew January <andrewjanuary@gmail.com>
2013-08-07 22:00:09 +0100

    Moves module description into class and splits work into smaller functions.

--- unpack.php
+++ unpack.php
@@ -1,19 +1,44 @@
 <?php
 	$archive_root = '/var/www/ivy/';
 	$cache_root = '/var/www/docs/cache/';
+
+	class ModuleDescription {
+		public $organisation, $module, $revision, $array, $isComplete = false;
+
+		public function __construct($uri) {
+			// Assumes the app is being hosted on the root of the domain
+			$uri_parts = explode('/', $uri);
+			
+			if (count($uri_parts) >= 2) {
+				$organisation = $uri_parts[1];
+				$array[] = $organisation;
+			}
+
+			if (count($uri_parts) >= 3) {
+				$module = $uri_parts[2];
+				$array[] = $module;
+			}
+
+			if (count($uri_parts) >= 4) {
+				$revision = $uri_parts[3];
+				$array[] = $revision;
+				$is_complete = true;
+			}
+		}
+	}
 	
-	function create_index($type, $module_description, $archive_root) {
-		$archive_path = $archive_root . '/' . join('/', $module_description);
+	function make_index($module_description, $archive_root) {
+		$archive_path = $archive_root . '/' . join('/', $module_description->array);
 
 		if (!is_dir($archive_path)) {
 			header("HTTP/1.0 404 Not Found");
 			return;
 		}
 
-		echo '<!doctype html><head><title>Docs - ' . $type . '</title></head><body><h1>Documentations</h1><h2>' . join(' / ', $module_description) . '</h2><ul>';
+		echo '<!doctype html><head><title>JivyDocs</title></head><body><h1>Documentations</h1><h2>' . join(' / ', $module_description->array) . '</h2><ul>';
  
 		foreach (scandir($archive_path) as $dir) {
-			$javadocs_pattern = $archive_path . '/' . $dir . str_repeat('/*', 2 - count($module_description)) . '/javadocs/*.jar';
+			$javadocs_pattern = $archive_path . '/' . $dir . str_repeat('/*', 2 - count($module_description->array)) . '/javadocs/*.jar';
 			if (count(glob($javadocs_pattern)) > 0) {
 				echo '<li><a href="' . $dir . '/">' . $dir . '</a></li>';
 			}
@@ -22,45 +47,31 @@
 		echo '</ul></body></html>';
 	}
 
-	// Assumes the app is being hosted on the root of the domain
-	$uri_parts = explode('/', $_SERVER['REQUEST_URI']);
+	function extract_javadocs($module_description, $archive_root, $cache_root) {
+		$archive_path = join('/', $module_description->array);
+		$javadoc_archive = $archive_root . '/' . $archive_path . '/javadocs/' . $module_description->module . '.jar';
+		$cache_path = $cache_root . '/' . $archive_path;
 
-	if (count($uri_parts) == 2) {
-		create_index('Organisations', array(), $archive_root);
-		return;
+		$zip = new ZipArchive;
+		if ($zip->open($javadoc_archive) !== TRUE) {
+			header("HTTP/1.0 404 Not Found");
+			return;
+		}
+
+		if (mkdir($cache_path, 0777, true)) {
+			$zip->extractTo($cache_path);
+			header('Location: ' . $_SERVER['REQUEST_URI']);
+		} else {
+			echo 'unpacking archive ' . $archive_path . ' to ' . $cache_path . ' failed';
+		}
+		$zip->close();
 	}
 
-	$organisation = $uri_parts[1];
+	$module_description = new ModuleDescription($_SERVER['REQUEST_URI']);
 
-	if (count($uri_parts) == 3) {
-		create_index('Modules', array($organisation), $archive_root);
-		return;
-	}
-
-	$module = $uri_parts[2];
-
-	if (count($uri_parts) == 4) {
-		create_index('Revisions', array($organisation, $module), $archive_root);
-		return;
-	}
-
-	$revision = $uri_parts[3];
-
-	$archive_path = $organisation . '/' . $module . '/' . $revision;
-	$javadoc_archive = $archive_root . '/' . $archive_path . '/javadocs/' . $module . '.jar';
-	$cache_path = $cache_root . '/' . $archive_path;
-
-	$zip = new ZipArchive;
-	if ($zip->open($javadoc_archive) !== TRUE) {
-		header("HTTP/1.0 404 Not Found");
-		return;
-	}
-
-	if (mkdir($cache_path, 0777, true)) {
-		$zip->extractTo($cache_path);
-		header('Location: '.$_SERVER['REQUEST_URI']);
+	if (!$module_description->isComplete) {
+		make_index($module_description, $archive_root);
 	} else {
-		echo 'unpacking archive ' . $archive_path . ' to ' . $cache_path . ' failed';
+		extract_javadocs($module_description, $archive_root, $cache_root);
 	}
-	$zip->close();
 ?>
